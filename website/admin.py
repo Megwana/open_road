77fe324473b8f2c9afee73916d8eb7339aa22343
Megan <megantech@hotmail.com>
2023-04-24 18:42:29 +0000

    Add: imported generic and models into view.py and saving work

--- website/admin.py
+++ website/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Post, Comment, Category
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
