56137f2e87c3c6013d737b892ed5f640fff7903f
Megan <megantech@hotmail.com>
2023-04-24 14:06:09 +0000

    Add: created initial models and created superuser which is working in the port.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=150, blank=True, null=True)
+    slug = models.SlugField(max_length=150, unique=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
