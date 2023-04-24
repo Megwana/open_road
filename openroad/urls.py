3c1033eeede951796791aa8cd84124fa8e29daa3
Megan <megantech@hotmail.com>
2023-04-24 20:45:27 +0000

    Added: Intial navigation bar

--- openroad/urls.py
+++ openroad/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include(website.urls), name="website-urls"),
+    path('', include(website.urls), name="website_urls"),
 ]
