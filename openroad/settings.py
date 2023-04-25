5f4e67dc6016cb903d571717c4cc57b3331492d3
Megan <megantech@hotmail.com>
2023-04-25 20:58:40 +0000

    Add: X Frame Options to allow summernote to work when deployed to Heroku

--- openroad/settings.py
+++ openroad/settings.py
@@ -19,6 +19,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['openroad.herokuapp.com', 'localhost']
 
 
