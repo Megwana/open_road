2527f5f34a5ace14af7d22a7b2b28cde0845f99a
Megan <megantech@hotmail.com>
2023-04-25 14:39:32 +0000

    Add: installed apps for allauth into settings.py and ran site to check login/ logout. It is successful.

--- openroad/urls.py
+++ openroad/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('website.urls'), name='website_urls'),
     path('roadtrips/', include('roadtrips.urls'), name='roadtrips_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
