e4b8c89e7c0f07be8be38e14aa886b408b49c80c
Megan <megantech@hotmail.com>
2023-04-25 20:32:19 +0000

    Add: alert messages when actions such as logging in are carried out. Message tags added to settings.py

--- openroad/settings.py
+++ openroad/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
     import env
 
@@ -47,6 +47,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-warning',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATES = 'bootstrap4'
 
