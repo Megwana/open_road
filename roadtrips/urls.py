9b006432a4b77681136cdc52d1338af7076219f8
Megan <megantech@hotmail.com>
2023-04-25 13:28:15 +0000

    Success: successfully showing visible post on road trips page

--- roadtrips/urls.py
+++ roadtrips/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('roadtrips/', views.PostList.as_view(), name='roadtrips'),
+    path('roadtrips', views.PostList.as_view(), name='roadtrips'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
