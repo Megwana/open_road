bba418d2f3bd7df6e987bfa00d22b7acb00da25b
Megan <megantech@hotmail.com>
2023-04-25 11:20:44 +0000

    Updated: Made migrations from 'website' to 'roadtrips' app and corrected links to show running roadtrip page

--- roadtrips/urls.py
+++ roadtrips/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='post_list'),
+    path('roadtrips/', views.PostList.as_view(), name='roadtrips'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
--- roadtrips/views.py
+++ roadtrips/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views import generic, View
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'trips.html'
+    template_name = 'roadtrips.html'
     paginate_by = 6
 
 
