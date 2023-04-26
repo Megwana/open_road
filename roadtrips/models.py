46c9fe7663ec2ce6b371586708025f0f39977a3e
Megan <megantech@hotmail.com>
2023-04-26 09:20:19 +0000

    Add: navbar link to add new post but only visible if a user is logged in

--- roadtrips/models.py
+++ roadtrips/models.py
@@ -25,12 +25,12 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=True, null=False)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=True, null=False)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
