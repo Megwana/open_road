f21043d3875d956f3f0950ae7073ca1c492d3d32
Megan <megantech@hotmail.com>
2023-09-08 15:16:16 +0000

    Add docstring explanations of code to make it easy to follow. Made sure all code meets PEP8 standards

--- roadtrips/views.py
+++ roadtrips/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from .models import Post, Category, Comment
+from django.contrib import messages
+from .models import Post
 from .forms import CommentForm, PostForm
 
 
 class PostList(ListView):
-    """connects Comment to ListView function """
+    """
+    Displays a list of posts.
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'roadtrips.html'
@@ -18,15 +19,17 @@ class PostList(ListView):
 
 
 class PostDetail(View):
-    """connects Post to View function """
-    def get(self, request, pk, *args, **kwards):
-        # status=1 to get only published posts
+    """
+    Displays the details of a post including comments and provides
+    functionality to add a new comment.
+    """
+
+    def get(self, request, pk, *args, **kwargs):
+        """Renders the post detail page."""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, pk=pk)
         comments = post.comments.filter(approved=True).order_by('created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        liked = post.likes.filter(id=request.user.id).exists()
 
         return render(
             request,
@@ -37,18 +40,15 @@ class PostDetail(View):
                 "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
-            },
+            }
         )
 
-    """Displays the comment section on the PostDetail View Page"""
-    def post(self, request, pk, *args, **kwards):
+    def post(self, request, pk, *args, **kwargs):
+        """Handles the creation of a new comment for the post."""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, pk=pk)
         comments = post.comments.filter(approved=True).order_by('created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
+        liked = post.likes.filter(id=request.user.id).exists()
         comment_form = CommentForm(data=request.POST)
 
         if comment_form.is_valid():
@@ -57,8 +57,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-        else:
-            comment_form = CommentForm()
+            messages.success(request, 'Comment added successfully!')
 
         return render(
             request,
@@ -68,59 +67,70 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "liked": liked,
-                "comment_form": CommentForm
-            },
+                "comment_form": CommentForm()
+            }
         )
 
 
-def form_valid(self, form):
-    """ validates form and connects to user """
-    comment_form.instance.created_by = self.request.user
-    return super().form_valid(form)
-
-
 class PostLike(View):
-    """connects PostLike to View function """
+    """
+    Handles the logic for users to like or unlike a post.
+    """
+
     def post(self, request, slug):
+        """Toggles the 'like' status for a post."""
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        return HttpResponseRedirect(reverse('post_detail', args=[post.pk]))
+        return HttpResponseRedirect(
+            reverse_lazy('post_detail', args=[post.pk])
+        )
 
 
 class PostCreate(CreateView):
-    """connects Comment to CreateView function """
+    """
+    Displays a form for creating a new post and saves it upon submission.
+    """
     model = Post
     form_class = PostForm
     template_name = 'post_form.html'
 
     def form_valid(self, form):
+        """Overrides form_valid to add a success message."""
         response = super().form_valid(form)
-        messages.success(self.request, 'Your post has been successfully created.')
+        messages.success(self.request, 'Post created successfully!')
         return response
 
+
 class PostUpdate(UpdateView):
-    """connects Post to UpdateView function """
+    """
+    Displays a form for updating an existing post and updates it upon
+    submission.
+    """
     model = Post
     form_class = PostForm
     template_name = 'post_update.html'
 
     def form_valid(self, form):
+        """Overrides form_valid to add a success message."""
         response = super().form_valid(form)
-        messages.success(self.request, 'Your post has been successfully updated.')
+        messages.success(self.request, 'Post updated successfully!')
         return response
 
 
 class PostDelete(DeleteView):
-    """connects Post to DeleteView function """
+    """
+    Displays a confirmation prompt before deleting a post and deletes it upon
+    confirmation.
+    """
     model = Post
-    form_class = PostForm
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
 
     def delete(self, request, *args, **kwargs):
+        """Overrides delete to add a success message."""
         response = super().delete(request, *args, **kwargs)
-        messages.success(self.request, 'Your post has been successfully deleted.')
+        messages.success(self.request, 'Post deleted successfully!')
         return response
