6ea99cd086f45946c2d680ac94e4bea7c4f8fb5e
Megan <megantech@hotmail.com>
2023-09-08 17:01:32 +0000

    Changed forms.py structure of lines to meet PEP8 standards

--- roadtrips/forms.py
+++ roadtrips/forms.py
@@ -1,6 +1,5 @@
 from .models import Comment, Post, Category
 from django import forms
-# from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 choices = Category.objects.all().values_list('name', 'name')
 
@@ -10,17 +9,23 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = fields = ['title', 'author', 'excerpt', 'category',
                            'featured_image', 'content']
-        # summernote_fields = ['content']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'hide-author', 'type':'hidden'}),
-            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'featured_image': forms.FileInput(attrs={'class': 'form-control-file'}),
+            'author': forms.TextInput(attrs={
+                'class': 'form-control',
+                'value': '',
+                'id': 'hide-author',
+                'type': 'hidden'
+            }),
+            'category': forms.Select(
+                choices=choices,
+                attrs={'class': 'form-control'}
+            ),
+            'featured_image': forms.FileInput(
+                attrs={'class': 'form-control-file'}
+            ),
             'excerpt': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
-            # 'content': SummernoteWidget(),
         }
 
 
