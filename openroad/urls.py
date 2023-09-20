b5a133c91dd5f2d57f6fc613879636aff2ffee6b
Megan <megantech@hotmail.com>
2023-09-20 13:27:56 +0000

    404 page added using html template and urls path

--- openroad/urls.py
+++ openroad/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'), name='website_urls'),
     path('roadtrips/', include('roadtrips.urls'), name='roadtrips_urls'),
     path('accounts/', include('allauth.urls')),
+    path('test_404/', TemplateView.as_view(template_name='404.html')),
 ]
