8f07bb3d42d0d0cf122d23228e8346d068bfa6ef
Megan <megantech@hotmail.com>
2023-09-20 12:52:24 +0000

    Add CommentModelTest to checks creation of comment and approval status for new comment

--- roadtrips/test_models.py
+++ roadtrips/test_models.py
@@ -12,6 +12,7 @@ class CategoryModelTest(TestCase):
 
 # Testing the Post model.
 class PostModelTest(TestCase):
+    # Common objects set up to be reused by all tests in this class.
     def setUp(self):
         self.user = User.objects.create_user(
             username='testuser', password='123456'
@@ -68,3 +69,48 @@ class PostModelTest(TestCase):
                 category=self.category,
                 content="Test Content"
                 )
+
+
+# Testing the Comment model.
+class CommentModelTest(TestCase):
+    # Common objects set up to be reused by all tests in this class.
+    def setUp(self):
+        # Create a test user.
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='123456'
+            )
+
+        # Create a test category.
+        self.category = Category.objects.create(name="Test Category")
+
+        # Create a test post.
+        self.post = Post.objects.create(
+            title="Test Post",
+            author=self.user,
+            category=self.category,
+            content="Test Content"
+            )
+
+    # Checks the creation of a comment instance.
+    def test_comment_creation(self):
+        # Create a new comment instance.
+        comment = Comment.objects.create(
+            post=self.post,
+            name="Test Name",
+            email="test@example.com",
+            body="Test Comment"
+            )
+        self.assertEqual(str(comment), "Comment Test Comment by Test Name")
+
+    # Checks the default approval status of a new comment.
+    def test_comment_approval(self):
+        # Create a new comment instance.
+        comment = Comment.objects.create(
+            post=self.post,
+            name="Test Name",
+            email="test@example.com",
+            body="Test Comment"
+            )
+        # Confirm that the comment's approval status is False by default.
+        self.assertFalse(comment.approved)
