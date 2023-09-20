fbada00d5fbe1ae1dad192e87141c0e8b132b29e
Megan <megantech@hotmail.com>
2023-09-20 13:30:18 +0000

    500 internal server error page created

--- openroad/urls.py
+++ openroad/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('roadtrips/', include('roadtrips.urls'), name='roadtrips_urls'),
     path('accounts/', include('allauth.urls')),
     path('test_404/', TemplateView.as_view(template_name='404.html')),
+    path('test_500/', TemplateView.as_view(template_name='500.html')),
 ]
