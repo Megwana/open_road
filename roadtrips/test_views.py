c00baf39bc2ec41265a215a2447c3fa64f81a85a
Megan <megantech@hotmail.com>
2023-09-22 14:15:32 +0000

    Add testing for views: post_list, post_detail, post_like, post_create, post_update and post_delete

--- roadtrips/test_views.py
+++ roadtrips/test_views.py
@@ -1,7 +1,8 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import Post
+from unittest.mock import patch
+from .models import Post, get_default_category
 from .views import (PostList, PostDetail, PostLike,
                     PostCreate, PostUpdate, PostDelete)
 
@@ -9,7 +10,6 @@ from .views import (PostList, PostDetail, PostLike,
 class PostViewTests(TestCase):
 
     def setUp(self):
-        self.factory = RequestFactory()
         self.user = User.objects.create_user(
             username='testuser', email='test@example.com', password='testpass'
         )
@@ -22,8 +22,62 @@ class PostViewTests(TestCase):
         self.client.login(username='testuser', password='testpass')
 
     def test_post_list_view(self):
-        request = self.factory.get(reverse('roadtrips'))
-        request.user = self.user
-        response = PostList.as_view()(request)
+        response = self.client.get(reverse('roadtrips'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Title')
+
+    def test_post_detail_view_get(self):
+        response = self.client.get(reverse('post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Title')
+
+    def test_post_detail_view_post(self):
+        form_data = {
+            'name': 'Test Commenter',
+            'email': 'commenter@test.com',
+            'content': 'Test Comment Content'
+        }
+        response = self.client.post(reverse(
+            'post_detail', args=[self.post.pk]), form_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_like_view(self):
+        response = self.client.post(
+            reverse('post_like', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_create_view(self):
+        form_data = {
+            'title': 'New Post',
+            'content': 'New Content',
+            'status': 1,
+            'author': self.user.id,
+            'category': get_default_category()
+        }
+
+        response = self.client.post(reverse('post_create'), form_data)
+
+        if response.context and 'form' in response.context:
+            print(response.context['form'].errors)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_update_view(self):
+        form_data = {
+            'title': 'Updated Title',
+            'content': 'Updated Content',
+            'status': 1,
+            'author': self.user.id,
+            'category': get_default_category()
+        }
+        response = self.client.post(reverse(
+            'post_update', args=[self.post.pk]), form_data)
+        print(response.status_code)
+
+        if response.context and 'form' in response.context:
+            print(response.context['form'].errors)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(
+            reverse('post_delete', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 302)
