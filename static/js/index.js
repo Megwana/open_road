cecd737fad3e8cdbf9aae21e38e1a878b9c7740b
Megan <megantech@hotmail.com>
2023-09-22 22:28:30 +0000

    Add Scroll function button, allows user to go back to top of the page. On home page and roadtrips page

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,11 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 10px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -17,4 +17,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
