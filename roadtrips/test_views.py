4b302e8e505b090c5abf782cdc8875b2c48b8cb9
Megan <megantech@hotmail.com>
2023-09-22 17:13:03 +0000

    Add comments for test_views explaining each test

--- roadtrips/test_views.py
+++ roadtrips/test_views.py
@@ -9,7 +9,7 @@ from .views import (PostList, PostDetail, PostLike,
 
 
 class PostViewTests(TestCase):
-
+    # Mock post set up
     def setUp(self):
         self.user = User.objects.create_user(
             username='testuser', email='test@example.com', password='testpass'
@@ -22,16 +22,19 @@ class PostViewTests(TestCase):
         )
         self.client.login(username='testuser', password='testpass')
 
+    # Test post list view
     def test_post_list_view(self):
         response = self.client.get(reverse('roadtrips'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Title')
 
+    # Test post details GET
     def test_post_detail_view_get(self):
         response = self.client.get(reverse('post_detail', args=[self.post.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Title')
 
+    # Test post details POST 
     def test_post_detail_view_post(self):
         form_data = {
             'name': 'Test Commenter',
@@ -42,6 +45,7 @@ class PostViewTests(TestCase):
             'post_detail', args=[self.post.pk]), form_data)
         self.assertEqual(response.status_code, 200)
 
+    # Test invalid Comments posted
     def test_invalid_comment_submission(self):
         form_data = {
             'name': '',
@@ -55,11 +59,36 @@ class PostViewTests(TestCase):
             'Failed to add comment. Please enter valid input.',
             response.content.decode())
 
+    # Test Comment successfully created when approved
+    def test_valid_comment_creation(self):
+        form_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(reverse(
+            'post_detail', args=[self.post.pk]), form_data)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the comment was added
+        self.assertTrue(
+            self.post.comments.filter(
+                body='This is a test comment.',
+                name=self.user,
+                email=self.user.email
+                ).exists())
+
+        # Check if the success message was added
+        messages_list = list(get_messages(response.wsgi_request))
+        self.assertIn(
+            'Comment added successfully. Waiting for approval!',
+            str(messages_list[0]))
+
+    # Test liking post view
     def test_post_like_view(self):
         response = self.client.post(
             reverse('post_like', args=[self.post.slug]))
         self.assertEqual(response.status_code, 302)
 
+    # Test unliking post view
     def test_post_unlike_view(self):
         self.post.likes.add(self.user)
         self.assertTrue(self.post.likes.filter(id=self.user.id).exists())
@@ -67,6 +96,7 @@ class PostViewTests(TestCase):
             reverse('post_like', args=[self.post.slug]))
         self.assertFalse(self.post.likes.filter(id=self.user.id).exists())
 
+    # Test Post Creation view
     def test_post_create_view(self):
         form_data = {
             'title': 'New Post',
@@ -99,6 +129,7 @@ class PostViewTests(TestCase):
         messages_list = list(get_messages(response.wsgi_request))
         self.assertIn(error_msg, str(messages_list[0]))
 
+    # Test Post Update view
     def test_post_update_view(self):
         form_data = {
             'title': 'Updated Title',
@@ -133,11 +164,13 @@ class PostViewTests(TestCase):
         messages_list = list(get_messages(response.wsgi_request))
         self.assertIn(error_msg, str(messages_list[0]))
 
+    # Test Post Delete view
     def test_post_delete_view(self):
         response = self.client.post(
             reverse('post_delete', args=[self.post.pk]))
         self.assertEqual(response.status_code, 302)
 
+    # temporary mock of a Post Deltion for test
     @patch(
         'roadtrips.models.Post.delete',
         side_effect=Exception('Forced DB Delete Exception'))
